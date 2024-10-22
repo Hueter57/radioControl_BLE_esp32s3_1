0400a4631d1fd7ae1ef3dee8b51a33f7ec7179fe
Hueter <kohsukemori@gmail.com>
2024-10-22 16:11:09 +0900

    create assign

---
+++ include/Assign.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Arduino.h>
+
+namespace assign {
+
+constexpr uint8_t LEFT_MOTOR_PWM_PIN1      = 0;
+constexpr uint8_t LEFT_MOTOR_PWM_PIN2      = 0;
+constexpr uint8_t LEFT_MOTOR_PWM_CH1       = 0;
+constexpr uint8_t LEFT_MOTOR_PWM_CH2       = 0;
+constexpr uint8_t LEFT_MOTOR_ENCODER_PIN1  = 0;
+constexpr uint8_t LEFT_MOTOR_ENCODER_PIN2  = 0;
+constexpr uint8_t RIGHT_MOTOR_PWM_PIN1     = 0;
+constexpr uint8_t RIGHT_MOTOR_PWM_PIN2     = 0;
+constexpr uint8_t RIGHT_MOTOR_PWM_CH1      = 0;
+constexpr uint8_t RIGHT_MOTOR_PWM_CH2      = 0;
+constexpr uint8_t RIGHT_MOTOR_ENCODER_PIN1 = 0;
+constexpr uint8_t RIGHT_MOTOR_ENCODER_PIN2 = 0;
+constexpr uint8_t LEFT_MOTOR_PWM_PIN1      = 0;
+
+}  // namespace assign
